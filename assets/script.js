92b0470e27a81d1e08c0bc3cf881eb469c549f03
Michael Samaras <mike@makemassair.co.uk>
2023-02-02 15:23:39 +0000

    current state of code build

--- assets/script.js
+++ assets/script.js
@@ -6,38 +6,24 @@ var weatherHistory = $("#history");
 let cityName = "";
 let searchArray = []; // declare variable as empty array
 
-$("#search-button").click(function(e) {
-    e.preventDefault();
-    var cityName = $("#search-input").val();
-    if (searchArray.includes(cityName) || cityName === "") {
-        return;
-    } else {
-        searchArray.push(cityName)
-        console.log(searchArray);
-        localStorage.setItem(`searches`, JSON.stringify(searchArray)); // puts the searched city into storage
-    
-    } 
-    
-    console.log(cityName);
+// get city weather
+function getCity() {
     var queryURL ="https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + API;
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(data) {
-        var lat = data.city.coord.lat; // pulls in lat
-        var lon = data.city.coord.lon; // pulls in lon
+        var lat = data.city.coord.lat; // pulls in latitude
+        var lon = data.city.coord.lon; // pulls in longitude
         var tempC = data.list[0].main.temp - 273.15; // converts Kelvin to Celsius
         var icon = data.list[0].weather[0].icon; // pulls icon code in
         var humidity = data.list[0].main.humidity; // pulls in humidity
         var windSpeed = data.list[0].wind.speed * 3.6; // pulls in wind speed (metre/sec) + converts to km/hr
         var iconImg = `https://openweathermap.org/img/wn/` + icon + `@2x.png`;
 
-        var queryURL5days = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API;
-
-        // prints weather icon
-        weatherToday.append($("<img>").attr("src", iconImg).addClass("icon"))
-
+        weatherToday.append($("<img>").attr("src", iconImg).addClass("icon")) // prints weather icon
+        
         // prints city name, adds today's date, puts in div class
         weatherToday.append($("<span>").text(data.city.name).append(today).addClass("currentCity"));
         weatherToday.append($("<span>").text("Temp: " + tempC.toFixed(2) + " °C").addClass("currentTemp"));
@@ -46,34 +32,54 @@ $("#search-button").click(function(e) {
         weatherToday.append($("<h2>").text(data.list[0].dt_txt));
         console.log(data);
         
-        $.ajax({
-            url: queryURL5days,
-            method: "GET"
-        }).then(function(data) {        
-            for (let i = 8; i < data.list.length; i++) {
-                if (i % 8 === 0 || i === data.list.length - 1) {
-                    var date = moment().add(i, 'day');
-                    var tempC = data.list[i].main.temp - 273.15; // converts Kelvin to Celsius
-                    var icon = data.list[i].weather[0].icon; // pulls icon code in
-                    var iconTxt = data.list[i].weather[0].description // pulls in weather description
-                    var iconImg = `https://openweathermap.org/img/wn/` + icon + `@2x.png`; // adds icon code to url
-                    var humidity = data.list[i].main.humidity; // pulls in humidity
-                    var windSpeed = data.list[i].wind.speed * 3.6; // pulls in wind speed + converts to km/hr
-                    // console.log(data);
-                    // console.log(date);
-                    weatherForecast.append(`<p class="date">${moment(data.list[i].dt_txt).format('dddd')}</p>`);
-                    weatherForecast.append($("<img>").attr("src", iconImg).attr("alt", iconTxt).addClass("icon"));
-                    weatherForecast.append($("<span>").text("Temp: " + tempC.toFixed(2) + " °C").addClass("currentTemp"));
-                    weatherForecast.append($("<span>").text("Wind : " + windSpeed.toFixed(2) + " km/h"));
-                    weatherForecast.append($("<span>").text("Humidity: " + humidity + " %"));
-                }
-            };
-        });
+        getForecast(lat, lon);
     });
+}
 
+// get 5 day forecast
+function getForecast(lat, lon) {
+    var queryURL5days = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API;
+
+    $.ajax({
+        url: queryURL5days,
+        method: "GET"
+    }).then(function(data) {        
+        for (let i = 8; i < data.list.length; i++) {
+            if (i % 8 === 0 || i === data.list.length - 1) {
+                var date = moment().add(i, 'day');
+                var tempC = data.list[i].main.temp - 273.15; // converts Kelvin to Celsius
+                var icon = data.list[i].weather[0].icon; // pulls icon code in
+                var iconTxt = data.list[i].weather[0].description // pulls in weather description
+                var iconImg = `https://openweathermap.org/img/wn/` + icon + `@2x.png`; // adds icon code to url
+                var humidity = data.list[i].main.humidity; // pulls in humidity
+                var windSpeed = data.list[i].wind.speed * 3.6; // pulls in wind speed + converts to km/hr
+                // console.log(data);
+                // console.log(date);
+                weatherForecast.append(`<p class="date">${moment(data.list[i].dt_txt).format('dddd')}</p>`);
+                weatherForecast.append($("<img>").attr("src", iconImg).attr("alt", iconTxt).addClass("icon"));
+                weatherForecast.append($("<span>").text("Temp: " + tempC.toFixed(2) + " °C").addClass("currentTemp"));
+                weatherForecast.append($("<span>").text("Wind : " + windSpeed.toFixed(2) + " km/h"));
+                weatherForecast.append($("<span>").text("Humidity: " + humidity + " %"));
+            }
+        };
+    });
+}
+
+// click event listener -> search button
+$("#search-button").click(function(e) {
+    e.preventDefault();
+    let cityName = $("#search-input").val();
+    if (searchArray.includes(cityName) || cityName === "") {
+        return;
+    } else {
+        searchArray.push(cityName);
+        console.log(searchArray);
+        localStorage.setItem(`searches`, JSON.stringify(searchArray)); // puts the searched city into storage
+    } 
+    // console.log(cityName);
+    getCity();
     searchHistoryRecall();
-    displaySearchHistory();
-
+    // weatherForecast.empty(); // clears out display of localStorage array
 });
 
 // retrieve searched cities
@@ -86,6 +92,8 @@ function searchHistoryRecall() {
     }
 };
 
+// searchHistoryRecall();
+
 // print searched cities    
 function displaySearchHistory(searchData) {
     for (let i = 0; i < searchData.length; i++) {
@@ -97,7 +105,11 @@ function displaySearchHistory(searchData) {
         
     }
 };
-searchHistoryRecall();
+
+// clear search results
+$('#clear-button').on("click", function(e){
+    localStorage.clear();
+});
 
 
 
